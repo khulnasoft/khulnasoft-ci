0a7039bc0ba1f9fb3218d364642bb403074291a2
Cacie Prins <cacie@cypress.io>
2025-01-14 14:17:39 -0500

    cypress-schematic eslint

---
+++ npm/cypress-schematic/eslint.config.ts
@@ -0,0 +1,23 @@
+import { baseConfig } from '../../eslint.config'
+import globals from 'globals'
+import path from 'path'
+
+export default [
+  ...baseConfig,
+  {
+    ignores: ['src/**/*.{js,js.map,d.ts}', 'src/schematics/cypress/files/cypress/plugins/*.js']
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['eslint.config.ts', 'projects/sandbox/src/fake-component.component.ts']
+        },
+        project: './tsconfig.json',
+      },
+      globals: {
+        ...globals.node,
+      }
+    }
+  }
+]
