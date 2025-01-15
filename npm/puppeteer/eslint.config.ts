63a0d72ec810643bcb6054a0691f83489293fc70
Cacie Prins <cacie@cypress.io>
2025-01-15 16:35:03 -0500

    fix puppeteer eslint

--- npm/puppeteer/eslint.config.ts
+++ npm/puppeteer/eslint.config.ts
@@ -1,3 +1,17 @@
 import { baseConfig } from '../../eslint.config'
 
-export default baseConfig
+export default [
+  ...baseConfig,
+  {
+    ignores: ['support/index.js'],
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['eslint.config.ts', 'cypress.config.ts'],
+        },
+      },
+    },
+  },
+]
